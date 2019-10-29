94d28eb6af6a3240b4469ad26e672130107cacab
gouwon <jskd2938@gmail.com>
2019-10-29 19:31:27 +0900

    191029 flaskr tutorial

---
+++ flaskr/auth.py
@@ -0,0 +1,84 @@
+import functools
+
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+## this will create a Blueprint named 'auth'. second argument is telling where it defined and url_prefix attribute will prepend to all the URLs.
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute('SELECT id FROM user WHERE username = ?', (username,)).fetchone() is not None:
+            error = 'User {} is already registerd.'.format(username)
+            ## db.execute takes a SQL query with ? placeholders for any user input, and a tuple of values to replace the placeholders with. The database library will take care of escaping the values so you are not vulnerable to a SQL injection attack
+
+        if error is None:
+            db.execute('INSERT INTO user (username, password) value(?, ?)', (username, generate_password_hash(password)))
+            db.commit()
+            return redirect(url_for('auth.login'))
+        
+        flash(error)
+        ## If validation fails, the error is shown to the user. flash() stores messages that can be retrieved when rendering the template.
+
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()
+
+        if user is None:
+            error = 'Incorrenct username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+            ## session is a dict that stores data across requests. The data is stored in a cookie that is sent to the browser, and the browser then sends it back with subsequent requests. Flask securely signs the data so that it can’t be tampered with.
+        
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
+
+    ## bp.before_app_request() registers a function that runs before the view function, no matter what URL is requested. load_logged_in_user checks if a user id is stored in the session and gets that user’s data from the database, storing it on g.user, which lasts for the length of the request. If there is no user id, or if the id doesn’t exist, g.user will be None.
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user in None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+
+    return wrapped_view
--- start_flask.py
+++ start_flask.py
@@ -1,3 +1,9 @@
-from flask_server import app
+# from flask_server import app
+# from flaskr import app
 
-app.run(host='0.0.0.0')  # 127.0.0.1 == localhost
+# app.run(host='0.0.0.0')  # 127.0.0.1 == localhost
+
+from flaskr import create_app
+
+app = create_app()
+app.run(host='0.0.0.0')
